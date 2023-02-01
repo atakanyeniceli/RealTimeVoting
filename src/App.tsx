cd6202aaadba7985c0b868231b72f98e300e4dc3
atakanyeniceli <atakanyeniceli@windowslive.com>
2023-02-01 21:24:22 +0300

    Frist Commit and Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from "@apollo/client";
+import { useState } from "react";
+import createApolloClient from "./Apollo/client";
+import Container from "./Components/Container";
+import Modal from "./Components/Modal";
 
 function App() {
+  const [client] = useState(createApolloClient());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="h-full overflow-x-hidden overflow-y-auto text-center relative">
+        <Modal />
+        <Container />
+      </div>
+    </ApolloProvider>
   );
 }
 
